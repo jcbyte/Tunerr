d0dc4c47fbabdaa56842e7887dbfd614d78fd36c
Joel Cutler <joelcutler108@gmail.com>
2025-02-18 16:59:12 +0000

    moved router to main app

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 
 import HomePage from "./HomePage";
 
+import { Navigate, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Room from "./Room";
+import RoomCreateUpdatePage from "./RoomCreateUpdatePage";
+import RoomJoinPage from "./RoomJoinPage";
+
 export default function App() {
+	const [roomCode, setRoomCode] = useState(null);
+
+	useEffect(() => {
+		fetch("/api/userInRoom")
+			.then((res) => res.json())
+			.then((data) => {
+				// setRoomCode(data.code);
+				console.log(data.code);
+			});
+	}, []);
+
+	function clearHomePageRoomCode() {
+		setRoomCode(null);
+	}
+
 	return (
 		<>
 			<div className="center">
-				<HomePage />
+				<Router>
+					<Routes>
+						<Route exact path="/" element={roomCode ? <Navigate to={"/room/" + roomCode} /> : <HomePage />} />
+						<Route path="/join" element={<RoomJoinPage />} />
+						<Route path="/create" element={<RoomCreateUpdatePage update={false} />} />
+						<Route path="/room/:roomCode" element={<Room clearHomePageRoomCode={clearHomePageRoomCode} />} />
+					</Routes>
+				</Router>
 			</div>
 		</>
 	);
